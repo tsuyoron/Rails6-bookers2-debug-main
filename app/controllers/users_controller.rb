548a3a190ba433d31f960f12195d09ab257b3dee
tsuyoron <108647290+tsuyoron@users.noreply.github.com>
2022-09-02 05:14:31 +0000

    Userinfoへの遷移修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,10 @@ class UsersController < ApplicationController
   def index
     @users = User.all
     @book = Book.new
+  end
 
   def edit
+    @user = User.find_by(params[:id])
   end
 
   def update
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: "homes#top"
+  root :to =>"homes#top"
   get "home/about"=>"homes#about"
 
   devise_for :users
@@ -9,5 +9,4 @@ Rails.application.routes.draw do
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
   resources :users, only: [:index,:show,:edit,:update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
 end
